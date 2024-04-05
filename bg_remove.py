61a435a32e21560db529cf9ede95be12b1d58025
rella <165660981+rella-freelance@users.noreply.github.com>
2024-04-05 17:16:53 +0300

    Update bg_remove.py

--- bg_remove.py
+++ bg_remove.py
@@ -1,9 +1,9 @@
 from rembg import remove
-from PIL import Image
+from PIL import Image #pillow 
 input_path = 'team1.jpg'
 output_path = 'team.jpg'
 
 inp = Image.open(input_path)
 output = remove(inp)
-output = output.convert("RGB")
+output = output.convert("RGB") #error raised if we do not convert encoding to RGB.
 output.save(output_path)
